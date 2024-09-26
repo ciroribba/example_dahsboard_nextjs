3498d2cb1c4d58f67e2672f39d4d8165e508647a
ciroribba <ciro.ribba@gmail.com>
2024-09-26 11:14:15 -0300

    add  edit invoices

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -13,6 +13,7 @@ import { redirect } from 'next/navigation';
   });
 
   const CreateInvoice = FormSchema.omit({ id: true, date: true });
+  
 
 export async function createInvoice(formData: FormData) {
     const { customerId, amount, status } = CreateInvoice.parse({
@@ -39,4 +40,26 @@ export async function createInvoice(formData: FormData) {
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
       
+}
+
+// Use Zod to update the expected types
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+ 
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
