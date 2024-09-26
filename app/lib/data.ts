617093674f998c3b73ed6d22f9cdd71f3b2dbaf8
ciroribba <ciro.ribba@gmail.com>
2024-09-26 13:39:52 -0300

    add errors

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -140,7 +140,7 @@ export async function fetchInvoicesPages(query: string) {
   }
 }
 
-export async function fetchInvoiceById(id: string) {
+export async function fetchInvoiceById(id: string) { 
   try {
     const data = await sql<InvoiceForm>`
       SELECT
@@ -157,7 +157,7 @@ export async function fetchInvoiceById(id: string) {
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
-
+    console.log(invoice);
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
